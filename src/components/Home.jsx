bc42a6d7adbf3259f0c5107811dfc0cf8746a726
Milos Jovanovic <milosjovanovic@yahoo.com>
2021-07-14 23:11:34 +0200

    added all

---
+++ src/components/Home.jsx
@@ -0,0 +1,63 @@
+/* import React, {useState} from 'react'
+
+//"onClick events working"//
+
+ const Home = () => {
+
+    const [name, setName] = useState('mario')
+    const [age, setAge] = useState(25);
+
+    const handleClick = () => {
+       setName('luigi');
+       setAge(30);
+    }
+    
+
+
+    const handleClickAgain = (name) =>{
+        console.log(`hello ` + name);
+    }
+
+    return (
+        <div className="home">
+            <h2>Homepage</h2>
+            <p>{name} is {age} years old</p>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={()=> handleClickAgain('mario')}>Click me again</button>
+        </div>
+    );
+}
+ export default Home; */
+
+import React from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
+const Home = () => {
+  /*   const [blogs, setBlogs] = useState([
+        {title:'My new website', body:'lorem ipsum...', author: 'mario', id:'1'},
+        {title:'Welcome party', body:'lorem ipsum...', author: 'yoshi', id:'2'},
+        {title:'Web dev top tips', body:'lorem ipsum...', author: 'mario', id:'3'},
+  
+    ]); */
+
+  /* const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+      }; */
+      const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
+
+  return (
+    <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && (<BlogList blogs={blogs} title="All my hope!!!" /* handleDelete={handleDelete} */
+        />
+      )}
+      {/*              <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title='Mario"s blog'/>
+       */}
+    </div>
+  );
+};
+
+export default Home;
